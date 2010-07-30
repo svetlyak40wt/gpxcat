4d4d67ff9f93769777f7c4dcd70c7b1b80955e14
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-07-30 14:07:02 +0400

    Added  setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'gpxcat',
+    version = '0.1.0',
+    description = 'Script to concatenate GPX tracks.',
+    keywords = 'gps gps navigation tracks',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/gpxcat/',
+    install_requires = 'lxml',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+    scripts = ['gpxcat.py'],
+)
